09fa5df1d51627ebfa1010ffdcdce7b8b38d8c08
Iwan0129 <uzunow0129@gmail.com>
2022-01-24 02:12:33 +0200

    Add API method documentation.

--- WarframeAPI/API.cs
+++ WarframeAPI/API.cs
@@ -16,6 +16,12 @@ namespace WarframeAPI
             PropertyNameCaseInsensitive = true
         };
 
+        /// <summary>
+        /// Parse json data to data type
+        /// </summary>
+        /// <typeparam name="T">The data type the json will be parsed to.</typeparam>
+        /// <param name="json">The json data that will be parsed.</param>
+        /// <returns>Parsed json to the data type if successful otherwise default value of the type.</returns>
         public static T Parse<T>(string json)
         {
             using JsonDocument jsonDocument = JsonDocument.Parse(json);
@@ -31,6 +37,13 @@ namespace WarframeAPI
             return JsonSerializer.Deserialize<T>(jsonData, JsonOptions);
         }
 
+        /// <summary>
+        /// Read data type from the API.
+        /// </summary>
+        /// <typeparam name="T">The data type to be read.</typeparam>
+        /// <param name="gamePlatform">The platfrom from which the data will be read.</param>
+        /// <param name="endPoint">The endpoint from which the data will be read.</param>
+        /// <returns>Parsed json to the data type if successful otherwise default value of the type.</returns>
         public static T Read<T>(GamePlatform gamePlatform = GamePlatform.Pc, string endPoint = "")
         {
             using WebClient client = new();
@@ -40,6 +53,13 @@ namespace WarframeAPI
             return !string.IsNullOrEmpty(json) ? Parse<T>(json) : default;
         }
 
+        /// <summary>
+        /// Read data type from the API async.
+        /// </summary>
+        /// <typeparam name="T">The data type to be read.</typeparam>
+        /// <param name="gamePlatform">The platfrom from which the data will be read.</param>
+        /// <param name="endPoint">The endpoint from which the data will be read.</param>
+        /// <returns>Parsed json to the data type if successful otherwise default value of the type.</returns>
         public static async Task<T> ReadAsync<T>(GamePlatform gamePlatform = GamePlatform.Pc, string endPoint = "")
         {
             using WebClient client = new();
@@ -49,6 +69,14 @@ namespace WarframeAPI
             return !string.IsNullOrEmpty(json) ? Parse<T>(json) : default;
         }
 
+        /// <summary>
+        /// Try to read data type from the API
+        /// </summary>
+        /// <typeparam name="T">The data type to be read.</typeparam>
+        /// <param name="data">The data type that the json will be parsed to. If the json isn't successfuly parsed then the default value of the type is set instead.</param>
+        /// <param name="gamePlatform">The platfrom from which the data will be read.</param>
+        /// <param name="endPoint">The endpoint from which the data will be read.</param>
+        /// <returns>True if the json is successfuly parsed to the data type otherwise false</returns>
         public static bool TryRead<T>(out T data, GamePlatform gamePlatform = GamePlatform.Pc, string endPoint = "")
         {
             using WebClient client = new();
